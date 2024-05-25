81b4370e8a32ddc5407f2f4143001c7d75e00506
Faishal Rasyid <faishalrasyid28@gmail.com>
2024-05-25 16:57:48 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello world!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam odit nostrum amet eius voluptatibus vitae quasi earum qui aut necessitatibus quidem rerum dolor, delectus, nihil minus, architecto adipisci mollitia ipsum?</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
